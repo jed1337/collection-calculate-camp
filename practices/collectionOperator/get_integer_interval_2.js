7e39f682852834d7870f8deb826fbdb256e3da24
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-09 18:45:13 +0800

    Partial completion of collectionOperators

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,29 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //write code here
+  let intervalList=[];
+
+  if(number_a<number_b){
+    for (let a = number_a; a <= number_b; a++) {
+      if(a%2!==0){
+        continue;
+      }
+      intervalList.push(a);    
+    }
+  } else{
+    for (let a = number_a; a >= number_b; a--) {
+      if(a%2!==0){
+        continue;
+      }
+      intervalList.push(a);
+    }
+  }
+
+  return intervalList;
+}
+
+function isOdd(number) {
+  return number%2!==0;
 }
 
 module.exports = get_integer_interval_2;
