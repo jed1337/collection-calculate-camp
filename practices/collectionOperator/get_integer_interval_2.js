2a489ddb331685a91dc36ec1283fc332d9df3c5a
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-09 18:51:38 +0800

    Refactor get_integer interval 2 to use higher order functions

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -3,27 +3,34 @@
 function get_integer_interval_2(number_a, number_b) {
   let intervalList=[];
 
-  if(number_a<number_b){
-    for (let a = number_a; a <= number_b; a++) {
-      if(a%2!==0){
-        continue;
-      }
-      intervalList.push(a);    
+  const operators = getOperators(number_a, number_b);
+
+  for (let a = number_a; operators.comparisonCondition(a, number_b); a+=operators.addValue) {
+    if(a%2!==0){
+      continue;
     }
-  } else{
-    for (let a = number_a; a >= number_b; a--) {
-      if(a%2!==0){
-        continue;
-      }
-      intervalList.push(a);
-    }
+    intervalList.push(a);    
   }
 
   return intervalList;
 }
 
-function isOdd(number) {
-  return number%2!==0;
+function getOperators(number_a, number_b){
+  if(number_a<number_b){
+    return {
+      comparisonCondition : function(a, b){
+        return a<=b;
+      },
+      addValue:1
+    }
+  } else{
+    return {
+      comparisonCondition : function(a,b){
+        return a>=b; 
+      },
+      addValue: -1
+    }
+  }
 }
 
 module.exports = get_integer_interval_2;
