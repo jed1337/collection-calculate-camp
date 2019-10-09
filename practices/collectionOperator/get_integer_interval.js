37c143f7a5ebea56e43d5794ba255c8f70dd79bd
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-09 19:36:32 +0800

    Finished letter interval 2. bad handling of edge cases

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,7 +1,33 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //write code here
+  let intervalList=[];
+
+  const operators = getOperators(number_a, number_b);
+
+  for (let a = number_a; operators.comparisonCondition(a, number_b); a+=operators.addValue) {
+    intervalList.push(a);    
+  }
+
+  return intervalList;
+}
+
+function getOperators(number_a, number_b){
+  if(number_a<number_b){
+    return {
+      comparisonCondition : function(a, b){
+        return a<=b;
+      },
+      addValue:1
+    }
+  } else{
+    return {
+      comparisonCondition : function(a,b){
+        return a>=b; 
+      },
+      addValue: -1
+    }
+  }
 }
 
 module.exports = get_integer_interval;
