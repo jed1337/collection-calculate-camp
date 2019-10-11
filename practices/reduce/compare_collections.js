425401f48d00224fa7c00e400dba69130722792c
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-11 20:42:00 +0800

    Finish compare collections.js

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,9 +1,18 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
-}
+  const distinctColA = Array.from(new Set(collection_a));
+  const distinctColB = Array.from(new Set(collection_b));
+
+  if(distinctColA.length!==distinctColB.length){
+    return false;
+  }
+
+  return collection_a
+    .reduce(function(acc, v1, idx){
+      let v2 = collection_b[idx];
+      return acc && v1===v2;
+    }, true);
+  }
 
 module.exports = compare_collections;
-
-
