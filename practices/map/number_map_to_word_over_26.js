c942db2d1bf2dae6c538cec142867f9c36c461a5
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-11 09:36:06 +0800

    Commit half of the map exercises

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,27 @@
 'use strict';
+const fromBackTick = 96;
+
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  // return ['a','m','aa','ad','y','aa'];
+
+  let wordList = [];
+  for(let element of collection){
+    let ones = element%26;
+    let tens = Math.floor(element/26);
+
+    let onesLetter = String.fromCharCode(fromBackTick+ones);
+    let tensLetter = String.fromCharCode(fromBackTick+tens);
+
+    if(tens === 0){
+      wordList.push(`${onesLetter}`);
+    } else if(tens>0 && ones === 0){
+      wordList.push(`${tensLetter}a`);
+    } else if(tens>0 && ones !==0){
+      wordList.push(`${tensLetter}${onesLetter}`);
+    }
+
+  }
+  return wordList;
 };
 
 module.exports = number_map_to_word_over_26;
