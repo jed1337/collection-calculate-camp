689ffb3f0a28490dbf4b469e97a4047e3df1c606
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-11 09:46:41 +0800

    Finish reduce to max/min test

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function collect_max_number(collection) {
-  //在这里写入代码
+  return collection
+    .reduce((a,b)=>Math.max(a,b));
 }
 
 module.exports = collect_max_number;
--- practices/reduce/collect_min_number.js
+++ practices/reduce/collect_min_number.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function collect_min_number(collection) {
-  //在这里写入代码
+  return collection
+    .reduce((a,b)=>Math.min(a,b));
 }
 
 module.exports = collect_min_number;
